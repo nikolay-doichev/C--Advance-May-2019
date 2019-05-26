54a79ecd617c654ec433ab94cc69bd1ce1e5398e
Николай Дойчев <nikolay.doichev@gmail.com>
2019-05-26 21:49:17 +0300

    Sets and Dictionaries Advance Lecture

---
+++ 03.SetsAndDictionariesAdvance_Lecture/03. Product Shop/03. Product Shop.cs
@@ -0,0 +1,44 @@
+﻿namespace _03._Product_Shop
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class StratUp
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, Dictionary<string, double>> shops = 
+                new SortedDictionary<string, Dictionary<string, double>>();
+
+            var entry = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            //lidl, juice, 2.30
+            
+            while (entry[0] != "Revision")
+            {
+                if (!shops.ContainsKey(entry[0]))
+                {
+                    shops.Add(entry[0], new Dictionary<string, double>());
+                }
+
+                if (!shops[entry[0]].ContainsKey(entry[1]))
+                {
+                    shops[entry[0]].Add(entry[1],0);
+                }
+                shops[entry[0]][entry[1]] = double.Parse(entry[2]);
+
+                entry = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var shop in shops)
+            {
+                Console.WriteLine($"{shop.Key}->");
+
+                foreach (var item in shop.Value)
+                {
+                    Console.WriteLine($"Product: {item.Key}, Price: {item.Value}");
+                    
+                }
+            }
+        }
+    }
+}
