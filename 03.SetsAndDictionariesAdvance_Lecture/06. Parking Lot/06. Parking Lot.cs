32829e85b705cbe0d15ca700fb1cbc015cae82a0
Николай Дойчев <nikolay.doichev@gmail.com>
2019-05-26 21:45:48 +0300

    Sets and Dictionaries Advance Lecture

---
+++ 03.SetsAndDictionariesAdvance_Lecture/06. Parking Lot/06. Parking Lot.cs
@@ -0,0 +1,41 @@
+﻿namespace _06._Parking_Lot
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class ParkingLot
+    {
+        static void Main(string[] args)
+        {
+            var entry = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            HashSet<string> carPlates = new HashSet<string>();
+
+            while (entry[0] != "END")
+            {
+                switch (entry[0])
+                {
+                    case "IN":
+                        carPlates.Add(entry[1]);
+                        break;
+                    case "OUT":
+                        carPlates.Remove(entry[1]);
+                        break;
+                    default:
+                        break;
+                }
+                entry = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            if (carPlates.Count>0)
+            {
+                foreach (var plate in carPlates)
+                {
+                    Console.WriteLine(plate);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
