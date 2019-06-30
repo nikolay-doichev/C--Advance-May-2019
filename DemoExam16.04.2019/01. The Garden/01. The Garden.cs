2ab15363093832117e354ff84452e1373ab87bce
Николай Дойчев <nikolay.doichev@gmail.com>
2019-06-30 13:10:52 +0300

    Demo Exam 16.04.2019

---
+++ DemoExam16.04.2019/01. The Garden/01. The Garden.cs
@@ -0,0 +1,134 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class TheGarden
+    {
+        static void Main(string[] args)
+        {
+            int numberRows = int.Parse(Console.ReadLine());
+            char[][] vegatebleField = new char[numberRows][];
+
+            Dictionary<char, int> harvestVegitables = new Dictionary<char, int>();
+            harvestVegitables['L'] = 0;
+            harvestVegitables['P'] = 0;
+            harvestVegitables['C'] = 0;
+
+            int harmedVegitables = 0;
+
+            for (int row = 0; row < numberRows; row++)
+            {
+                //char[] inputVegatables = Console.ReadLine().Split(new char[' '],StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                string[] inputVegatables = Console.ReadLine().Split(' ');
+                vegatebleField[row] = new char[inputVegatables.Length];
+                for (int cols = 0; cols < vegatebleField[row].Length; cols++)
+                {
+                    vegatebleField[row][cols] = char.Parse(inputVegatables[cols]);
+                }
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "End of Harvest")
+            {
+                string[] commandLine = input.Split(' ');
+                //Harvest 0 2
+                if (commandLine[0] == "Harvest")
+                {
+                    int rowHarvest = int.Parse(commandLine[1]);
+                    int colHarvest = int.Parse(commandLine[2]);
+
+                    if (rowHarvest<numberRows && colHarvest< vegatebleField[rowHarvest].Length)
+                    {
+                        char vegitable = vegatebleField[rowHarvest][colHarvest];
+
+                        if (vegitable == 'L' || vegitable == 'P' || vegitable == 'C')
+                        {
+                            vegatebleField[rowHarvest][colHarvest] = ' ';
+
+                            harvestVegitables[vegitable]++;
+                        }
+                    }
+                }
+                //Mole 2 2 up
+                else if (commandLine[0] == "Mole")
+                {
+                    int rowMole = int.Parse(commandLine[1]);
+                    int colMole = int.Parse(commandLine[2]);
+                    string direction = commandLine[3];
+
+                    if (rowMole < numberRows && colMole < vegatebleField[rowMole].Length)
+                    {
+                        if (vegatebleField[rowMole][colMole] != ' ')
+                        {
+                            vegatebleField[rowMole][colMole] = ' ';
+                            harmedVegitables++;
+                        }                        
+
+                        switch (direction)
+                        {
+                            case "up":
+                                for (int i = rowMole ; i >= 0; i-=2)
+                                {
+                                    if (vegatebleField[i][colMole] != ' ')
+                                    {
+                                        vegatebleField[i][colMole] = ' ';
+                                        harmedVegitables++;
+                                    }                                    
+                                }
+                                break;
+                            case "down":
+                                for (int i = rowMole; i < numberRows; i+=2)
+                                {
+                                    if (vegatebleField[i][colMole] != ' ')
+                                    {
+                                        vegatebleField[i][colMole] = ' ';
+                                        harmedVegitables++;
+                                    }
+                                }
+                                break;
+                            case "left":
+                                for (int i = colMole ; i >= 0; i-=2)
+                                {
+                                    if (vegatebleField[rowMole][i] != ' ')
+                                    {
+                                        vegatebleField[rowMole][i] = ' ';
+                                        harmedVegitables++;
+                                    }
+                                }
+                                break;
+                            case "right":
+                                for (int i = colMole; i < vegatebleField[rowMole].Length; i+=2)
+                                {
+                                    if (vegatebleField[rowMole][i] != ' ')
+                                    {
+                                        vegatebleField[rowMole][i] = ' ';
+                                        harmedVegitables++;
+                                    }
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            for (int row = 0; row < numberRows; row++)
+            {
+                for (int col = 0; col < vegatebleField[row].Length; col++)
+                {
+                    Console.Write(vegatebleField[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Carrots: {harvestVegitables['C']}");
+            Console.WriteLine($"Potatoes: {harvestVegitables['P']}");
+            Console.WriteLine($"Lettuce: {harvestVegitables['L']}");
+            Console.WriteLine($"Harmed vegetables: {harmedVegitables}");
+        }
+    }
+}
---
+++ DemoExam16.04.2019/02. Make a Salad/02. Make a Salad.cs
@@ -0,0 +1,80 @@
+﻿namespace _02._Make_a_Salad
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class MakeSalad
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> vegitables = new Queue<string>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries));
+
+            Stack<int> saladsCalories = new Stack<int>(Console.ReadLine()
+                                         .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                         .Select(int.Parse));
+            int tomatoCalories = 80;
+            int carrotCalories = 136;
+            int lettuceCalories = 109;
+            int potatoCalories = 215;
+
+
+
+            while (vegitables.Count>0 && saladsCalories.Count>0)
+            {
+                int currentSaladCalories = saladsCalories.Peek();
+                string currentVegitable = vegitables.Peek();
+                int calories = 0;
+                
+                while (currentSaladCalories>0)
+                {
+                    switch (currentVegitable)
+                    {
+                        case "tomato":
+                            calories = tomatoCalories;
+                            break;
+                        case "carrot":
+                            calories = carrotCalories;
+                            break;
+                        case "lettuce":
+                            calories = lettuceCalories;
+                            break;
+                        case "potato":
+                            calories = potatoCalories;
+                            break;
+                    }
+                    //112 calories tomato = 80
+                    if (currentSaladCalories - calories > 0)
+                    {
+                        currentSaladCalories -= calories;
+                        vegitables.Dequeue();
+                        if (vegitables.Count == 0)
+                        {
+                            int result = saladsCalories.Pop();
+                            Console.Write(result + " ");
+                            break;
+                        }
+                        currentVegitable = vegitables.Peek();
+                    }
+                    //32 callories left potatto
+                    else
+                    {
+                        vegitables.Dequeue();
+                        int result = saladsCalories.Pop();
+                        Console.Write(result + " ");
+                        break;
+                    }
+                }
+                
+            }
+            Console.WriteLine();
+            foreach (var item in saladsCalories)
+            {
+                Console.Write(item + " ");
+            }
+            foreach (var item in vegitables)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
